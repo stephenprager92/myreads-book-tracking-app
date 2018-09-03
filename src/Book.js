ecffca587be0007f3234b323255a4c5da727f7ff
Steve Prager <stephenprager92@gmail.com>
2018-09-03 17:04:24 -0400

    Add BookShelf component

--- src/Book.js
+++ src/Book.js
@@ -12,14 +12,15 @@ class Book extends React.Component {
     /* Book requires a title, an author, and an image URL as props */
 	static propTypes = {
 		title: PropTypes.string.isRequired,
-		author: PropTypes.string.isRequired
+		author: PropTypes.string.isRequired,
+		width: PropTypes.num.isRequired,
 		imageURL: PropTypes.string.isRequired
 	}
 
 	render() {
 		return <div className="book">
 	               <div className="book-top">
-	                   <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+	                   <div className="book-cover" style={{ width: {/*128*/}, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
                        <div className="book-shelf-changer">
 	                       <select>
 	                           <option value="move" disabled>Move to...</option>
